ff988676b00117fed0630d5837ed21bb171fe9e0
Colin <colin.hill728@gmail.com>
2018-07-23 14:08:57 +0200

    [fix] linter warnings

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,32 @@
 const webpack = require('webpack');
 
-module.exports = { 
+module.exports = {
   entry: [
     'react-hot-loader/patch',
-    './src/index.js'
+    './src/index.js',
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
+        use: ['babel-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx'],
   },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
-    filename: 'bundle.js'
-  }, 
+    filename: 'bundle.js',
+  },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
   ],
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: './dist',
+    port: 3000,
+  },
 };
